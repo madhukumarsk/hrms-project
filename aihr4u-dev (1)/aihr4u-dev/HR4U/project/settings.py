6e31d038e86723406bd8f6297979687c49f391ee
madhukumarsk <40524588+madhukumarsk@users.noreply.github.com>
2025-04-01 16:40:11 +0530

    Update settings.py

--- aihr4u-dev (1)/aihr4u-dev/HR4U/project/settings.py
+++ aihr4u-dev (1)/aihr4u-dev/HR4U/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*^vb_x5q7pck_rk*lwby^s2--&y902lpa71=11ix2miyeut+$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
